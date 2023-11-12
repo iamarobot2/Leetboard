afe4f3c417eddac58edd750a87d66c24d0f21c54
iamarobot2 <avinjoshy21@gmail.com>
2023-11-12 17:28:42 +0530

    deploy

--- api.js
+++ api.js
@@ -1,1 +1,83 @@
+var users = [
+    {
+        rank:1,
+        name:"Avin Joshy",
+        username:"iamarobot2129",
+        questionsolved:0,
+    },
+    {
+        rank:0,
+        name:"Arjun Saji",
+        username:"ARJUN_SAJI",
+        questionsolved:0,
+    },
+    {
+        rank:0,
+        name:"Elbin Santhosh",
+        username:"Elbin001",
+        questionsolved:0,
+    },
+    {
+        rank:0,
+        name:"Anandhu Anoj",
+        username:"anandhu_anoj",
+        questionsolved:0,
+    },
+    {
+        rank:0,
+        name:"Augustine salas",
+        username:"",
+        questionsolved:0,
+    },
+    {
+        rank:0,
+        name:"Arun KA",
+        username:"",
+        questionsolved:0,
+    }
+]
+users.sort((a,b)=>{
+    if(a.name < b.name)
+    {
+        return -1;
+    }
+    if(a.name > b.name)
+    {
+        return 1;
+    }
+    return 0;
+});
+users.sort((a,b)=>b.rank-a.rank);
+var tbdy = document.getElementById('tbody');
+for(i=0;i<users.length;i++)
+{
+    var tr = document.createElement('tr');
+    for(let val in users[i])
+    {
+        let td = document.createElement('td');
+        td.innerHTML=users[i][val];
+        tr.append(td);
+    }
+    tbdy.appendChild(tr)
+}
+
+fetch('https://leetboard-1yoxti5gy-avin-joshys-projects.vercel.app')
+.then(response => response.json())
+.then(usersData => {
+    usersData.forEach(userData => {
+        let user = users.find(user => user.username === userData.username);
+        if(user) {
+            user.questionsolved = userData.totalSolved;
+        }
+    });
+
+    users.sort((a, b) => b.questionsolved - a.questionsolved);
+    for(let i = 0; i < users.length; i++) {
+        users[i].rank = i + 1;
+        let tr = tbdy.children[i];
+        tr.children[0].innerHTML = users[i].rank;
+        tr.children[3].innerHTML = users[i].questionsolved;
+    }
+})
+.catch(error => console.error('Error:', error));
 
---
+++ index.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors({
+    origin: '*'
+}));
+
+let users = [
+    {
+        rank:1,
+        name:"Avin Joshy",
+        username:"iamarobot2129",
+        questionsolved:0,
+    },
+    {
+        rank:0,
+        name:"Arjun Saji",
+        username:"ARJUN_SAJI",
+        questionsolved:0,
+    },
+    {
+        rank:0,
+        name:"Elbin Santhosh",
+        username:"Elbin001",
+        questionsolved:0,
+    },
+    {
+        rank:0,
+        name:"Anandhu Anoj",
+        username:"anandhu_anoj",
+        questionsolved:0,
+    },
+    {
+        rank:0,
+        name:"Augustine salas",
+        username:"",
+        questionsolved:0,
+    },
+    {
+        rank:0,
+        name:"Arun KA",
+        username:"",
+        questionsolved:0,
+    }
+];
+
+const query = `
+  query getUserProfile($username: String!) {
+    allQuestionsCount {
+      difficulty
+      count
+    }
+    matchedUser(username: $username) {
+      contributions {
+        points
+      }
+      profile {
+        reputation
+        ranking
+      }
+      submissionCalendar
+      submitStats {
+        acSubmissionNum {
+          difficulty
+          count
+          submissions
+        }
+        totalSubmissionNum {
+          difficulty
+          count
+          submissions
+        }
+      }
+    }
+  }
+`;
+
+app.get('/', (req, res) => {
+    res.send(`<b>API URL:</b>/<b style="color:crimson;">${req.hostname}</b>`)
+});
+
+app.get('/fetchUsersData', (req, res) => {
+    let usersData = [];
+    let promises = users.map(user => {
+        if(user.username) {
+            return import('node-fetch').then(fetch => {
+                return fetch.default(`https://leetcode.com/graphql`, {
+                    method: 'POST',
+                    headers: { 
+                        'Content-Type': 'application/json',
+                        'Referer': 'https://leetcode.com'
+                    },
+                    body: JSON.stringify({
+                        query: query,
+                        variables: {username: user.username}
+                    }),
+                })
+                .then(response => response.json())
+                .then(data => {
+                    if(!data.errors){
+                        usersData.push({
+                            username: user.username,
+                            totalSolved: data.data.matchedUser.submitStats.acSubmissionNum[0].count,
+                        });
+                    }
+                })
+                .catch(error => console.error('Error:', error));
+            });
+        }
+    });
+
+    Promise.all(promises).then(() => res.send(usersData));
+});
+
+const port = process.env.PORT || 3500;
+app.listen(port, () => {
+    console.log(`App is running on port ${port}`);
+});
